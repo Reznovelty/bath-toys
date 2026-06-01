60131ce1febf49a1c05ab2d43cb98fcfe2bd4df6
Reznovelty <139622774+Reznovelty@users.noreply.github.com>
2026-06-01 21:10:41 +0100

    some changes

---
+++ Assets/Scripts/RezWaterManager.cs
@@ -0,0 +1,34 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class RezWaterManager : MonoBehaviour
+{
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+
+    private Vector3[] baseVertices;
+    private Vector3[] displacedVertices;
+
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        baseVertices = meshFilter.mesh.vertices;
+        displacedVertices = new Vector3[baseVertices.Length];
+    }
+
+    private void Update()
+    {
+        Vector3[] vertices = meshFilter.mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = RezWaveManager.instance.GetWaveHeight(transform.position.x + vertices[i].x);
+        }
+
+        meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.RecalculateNormals();
+    }
+}
